c27be15a916938fa40cd6fb0e7c875f6aecb0bc3
Jakub Ondrák <jakub.ondrak@siemens.com>
2022-01-25 17:31:53 +0100

    Added MovieModel

---
+++ FilmotekaLibrary/Models/MovieModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmotekaLibrary.Models
+{
+    public class MovieModel
+    {
+        public int Id { get; set; }
+        public string OriginalName { get; set; }
+        public string NationalName { get; set; }
+        public int Year { get; set; }
+        public bool IsWatched { get; set; }
+        public bool IsNotRelevant { get; set; }
+
+
+    }
+}
