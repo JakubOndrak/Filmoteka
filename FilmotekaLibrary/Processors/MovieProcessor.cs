2b85273754bd04fd65f574582baae3aca5da0fda
Jakub Ondrák <jakub.ondrak@siemens.com>
2022-01-29 17:18:47 +0100

    modification of MovieModel

---
+++ FilmotekaLibrary/Processors/MovieProcessor.cs
@@ -0,0 +1,21 @@
+﻿using FilmotekaLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmotekaLibrary.Processors
+{
+    public class MovieProcessor
+    {
+        private readonly List<MovieModel> moviesInDb = new ();
+
+        public MovieProcessor()
+        {
+            
+        }
+
+        
+    }
+}
