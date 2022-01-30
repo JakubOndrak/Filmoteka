a0451ad45af5bcb9ba40abb92976010cf4cfca4a
Jakub Ondrák <jakub.ondrak@siemens.com>
2022-01-30 18:05:30 +0100

    added DateWatchedModel

---
+++ FilmotekaLibrary/Models/DateWatchedModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmotekaLibrary.Models
+{
+    public class DateWatchedModel
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public DateTime DateWatched { get; set; }
+    }
+}
