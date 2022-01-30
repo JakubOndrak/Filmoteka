b4ce1514e9921bfa1ec46775201018be43c19f03
Jakub Ondrák <jakub.ondrak@siemens.com>
2022-01-30 18:29:55 +0100

    set up of constructor of MovieProcessor

--- FilmotekaLibrary/Processors/MovieProcessor.cs
+++ FilmotekaLibrary/Processors/MovieProcessor.cs
@@ -1,4 +1,5 @@
-﻿using FilmotekaLibrary.Models;
+﻿using FilmotekaLibrary.DataAccess;
+using FilmotekaLibrary.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,16 @@ namespace FilmotekaLibrary.Processors
 {
     public class MovieProcessor
     {
+        private readonly IFilmotekaDataAccess filmotekaDataAccess = new MockDataAccess();
         private readonly List<MovieModel> moviesInDb = new ();
+        private readonly List<FolderModel> movieFolders = new ();
+        private readonly List<FileExtensionModel> fileExtensions = new();
 
         public MovieProcessor()
         {
-            
+            moviesInDb = filmotekaDataAccess.GetAllMoviesFromDb();
+            movieFolders = filmotekaDataAccess.GetAllFolders();
+            fileExtensions = filmotekaDataAccess.GetAllFileExtentions();
         }
 
         
